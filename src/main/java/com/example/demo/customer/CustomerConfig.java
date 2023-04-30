9e3eed0894357a502a7e537e51f98fa41e336c7b
jason.jin <jasonjin1107@hotmail.com>
2023-04-30 21:29:55 +1200

    multiple application files added

---
+++ src/main/java/com/example/demo/customer/CustomerConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.customer;
+
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class CustomerConfig {
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return args -> {
+            System.out.println(infoApp);
+            System.out.println("company name is " + companyName);
+            System.out.println("using springboot environment property " + environment.getProperty("info.app.name"));
+        };
+    }
+}
